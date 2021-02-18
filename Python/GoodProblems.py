0898acf4b50dbf086b89ecbde440c9475c670753
Nhan Duong <nhanduong@mail.usf.edu>
2021-02-18 09:26:49 -0500

    Learning ABSWP

--- Python/GoodProblems.py
+++ Python/GoodProblems.py
@@ -1,5 +1,3 @@
-from collections import Counter
-
 #The groups_per_user function receives a dictionary, which contains group names with the list of users.
 #Users can belong to multiple groups.
 #Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values.
@@ -620,4 +618,13 @@ def viralAdvertising(n):
 	return cumulative
 
 print(viralAdvertising(5))
-		 
+
+'''
+A jail has a number of prisoners and a number of treats to pass out to them. 
+Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. 
+A chair number will be drawn from a hat. 
+Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
+The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. 
+Determine the chair number occupied by the prisoner who will receive that candy.
+'''
+def saveThePrisoner(n, m ,s):
